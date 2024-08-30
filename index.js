507b680dd754240aa067afda5cdff27e8f50fd88
Marc Valeros <marcnathanielvaleros@gmail.com>
2024-08-30 17:20:42 +0800

    import file to external api, in progress

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 const axios = require('axios');
 const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+const FormData = require('form-data');
 
 //create a server 
 const app = express();
+const port = process.env.PORT || 8080;
+
+
+// Configure multer to use memory storage
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 
 app.get('/', (req, res) => {
   res.send('Your server is running.');
@@ -13,21 +24,56 @@ app.get('/', (req, res) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/api/import', async (req, res) => {
+// Configure CORS to allow requests from your frontend
+app.use(cors({
+  origin: 'http://localhost:3000',    //replace during prod
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.post('/api/import', upload.array('files', 5), async (req, res) => {
   try {
-    const response = await axios.post('https://api.hubapi.com/crm/v3/imports/', req.body, {
-      headers: {
-        'Authorization': `Bearer ${process.env.HUBSPOT_API_KEY}`,
-        'Content-Type': 'multipart/form-data'
-      }
+    console.log('Files Details:', req.files);
+    console.log('Body:', req.body);
+    console.log('Body Details:', req.body.importRequest);
+
+    let data = new FormData();
+
+    // req.files.forEach(file => {
+    //   console.log(`File: ${file.originalname}, Size: ${file.buffer.length}`);
+    //   console.log(`Content (utf8):\n${file.buffer.toString('utf8')}`);
+    // });
+
+    data.append('importRequest', req.body.importRequest);
+    req.files.forEach(file => {
+      data.append('files', file.buffer, {filename:file.originalname, contentType: 'text/csv'});
     });
-    res.json(response.data);
+    
+  
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://api.hubapi.com/crm/v3/imports',
+      headers: { 
+        // 'Content-Type': 'multipart/form-data', 
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${process.env.HUBSPOT_API_KEY}`, 
+        ...data.getHeaders()
+      },
+      data : data
+    };
+
+    const response = await axios.request(config);
+    console.log(JSON.stringify(response.data));
+    // res.status(200).json(response.data);
+    res.status(200).send("Successfully Added File");
   } catch (error) {
+    console.error(error.response ? error.response.data : error.message);
     res.status(error.response ? error.response.status : 500).send(error.response ? error.response.data : 'Server Error');
   }
 });
 
-const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 })
